e3ba66a6d2ace51eaa21da6092c6c1b998f99eb1
Yasmin elbanna <yasminzaki1911@gmail.com>
2024-02-17 23:06:17 +0200

    solving cors bug

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const cors = require('cors');
 const bodyParser=require('body-parser')
 const morgan=require('morgan')
 const app=express();
+
 app.use(cors({
-    origin:"http://localhost:8000",
-    methods:["GET","POST","DELETE","PUT","PATCH"],
+    origin:"*",
 }));
+
 app.use(morgan('dev'))
 
 const publicerror=require('./errors/errors')
